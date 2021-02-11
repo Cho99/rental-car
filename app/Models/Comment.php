d2e61a94979c24e67bc0fbe5ed41c54bd42eac8b
Cho99 <quangsoi99@gmail.com>
2021-02-11 09:11:41 +0700

    Relation Database

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Request;
+use App\Models\Car;
 
 class Comment extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
